d67b10411e1e9f9294c51aaf2e55524ac239bdae
binghe001 <1028386804@qq.com>
2022-10-17 12:05:28 +0800

    第18章：服务消费者整合动态代理实现直接调用接口返回结果数据

---
+++ bhrpc-consumer/bhrpc-consumer-native/src/main/java/io/binghe/rpc/consumer/RpcClient.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2020-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.rpc.consumer;
+
+import io.binghe.rpc.common.threadpool.ClientThreadPool;
+import io.binghe.rpc.consumer.common.RpcConsumer;
+import io.binghe.rpc.proxy.jdk.JdkProxyFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * @author binghe(公众号：冰河技术)
+ * @version 1.0.0
+ * @description 服务消费客户端
+ */
+public class RpcClient {
+
+    private final Logger logger = LoggerFactory.getLogger(RpcClient.class);
+    /**
+     * 服务版本
+     */
+    private String serviceVersion;
+    /**
+     * 服务分组
+     */
+    private String serviceGroup;
+    /**
+     * 序列化类型
+     */
+    private String serializationType;
+    /**
+     * 超时时间
+     */
+    private long timeout;
+
+    /**
+     * 是否异步调用
+     */
+    private boolean async;
+
+    /**
+     * 是否单向调用
+     */
+    private boolean oneway;
+
+    public RpcClient(String serviceVersion, String serviceGroup, String serializationType, long timeout, boolean async, boolean oneway) {
+        this.serviceVersion = serviceVersion;
+        this.timeout = timeout;
+        this.serviceGroup = serviceGroup;
+        this.serializationType = serializationType;
+        this.async = async;
+        this.oneway = oneway;
+    }
+
+    public <T> T create(Class<T> interfaceClass) {
+        JdkProxyFactory<T> jdkProxyFactory = new JdkProxyFactory<T>(serviceVersion, serviceGroup, serializationType, timeout, RpcConsumer.getInstance(), async, oneway);
+        return jdkProxyFactory.getProxy(interfaceClass);
+    }
+
+    public void shutdown() {
+        RpcConsumer.getInstance().close();
+    }
+}
---
+++ bhrpc-test/bhrpc-test-consumer/bhrpc-test-consumer-native/src/main/java/io/binghe/test/consumer/RpcConsumerNativeTest.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2020-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.test.consumer;
+
+import io.binghe.rpc.consumer.RpcClient;
+import io.binghe.rpc.test.api.DemoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author binghe(公众号：冰河技术)
+ * @version 1.0.0
+ * @description 测试Java原生启动服务消费者
+ */
+public class RpcConsumerNativeTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RpcConsumerNativeTest.class);
+
+    public static void main(String[] args){
+        RpcClient rpcClient = new RpcClient("1.0.0", "binghe", "jdk", 3000, false, false);
+        DemoService demoService = rpcClient.create(DemoService.class);
+        String result = demoService.hello("binghe");
+        LOGGER.info("返回的结果数据===>>> " + result);
+        rpcClient.shutdown();
+    }
+}
