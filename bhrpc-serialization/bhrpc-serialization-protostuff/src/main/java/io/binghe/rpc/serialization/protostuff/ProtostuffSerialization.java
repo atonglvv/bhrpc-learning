11e2fe0347ac80edc51ea502b43baae4a030322b
binghe001 <1028386804@qq.com>
2022-11-04 11:44:40 +0800

    第31章：基于SPI扩展Protostuff序列化与反序列化机制

---
+++ bhrpc-serialization/bhrpc-serialization-protostuff/src/main/java/io/binghe/rpc/serialization/protostuff/ProtostuffSerialization.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2020-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.rpc.serialization.protostuff;
+
+import com.dyuproject.protostuff.LinkedBuffer;
+import com.dyuproject.protostuff.ProtostuffIOUtil;
+import com.dyuproject.protostuff.Schema;
+import com.dyuproject.protostuff.runtime.RuntimeSchema;
+import io.binghe.rpc.common.exception.SerializerException;
+import io.binghe.rpc.serialization.api.Serialization;
+import io.binghe.rpc.spi.annotation.SPIClass;
+import org.objenesis.Objenesis;
+import org.objenesis.ObjenesisStd;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author binghe(公众号：冰河技术)
+ * @version 1.0.0
+ * @description rotostuff序列化
+ */
+@SPIClass
+public class ProtostuffSerialization implements Serialization {
+
+    private final Logger logger = LoggerFactory.getLogger(ProtostuffSerialization.class);
+
+    private Map<Class<?>, Schema<?>> cachedSchema = new ConcurrentHashMap<>();
+
+    private Objenesis objenesis = new ObjenesisStd(true);
+
+    @SuppressWarnings("unchecked")
+    private <T> Schema<T> getSchema(Class<T> cls) {
+        Schema<T> schema = (Schema<T>) cachedSchema.get(cls);
+        if (schema == null) {
+            schema = RuntimeSchema.createFrom(cls);
+            if (schema != null) {
+                cachedSchema.put(cls, schema);
+            }
+        }
+        return schema;
+    }
+
+    /**
+     * 序列化（对象 -> 字节数组）
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> byte[] serialize(T obj) {
+        logger.info("execute protostuff serialize...");
+        if (obj == null){
+            throw new SerializerException("serialize object is null");
+        }
+        Class<T> cls = (Class<T>) obj.getClass();
+        LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+        try {
+            Schema<T> schema = getSchema(cls);
+            return ProtostuffIOUtil.toByteArray(obj, schema, buffer);
+        } catch (Exception e) {
+            throw new SerializerException(e.getMessage(), e);
+        } finally {
+            buffer.clear();
+        }
+    }
+
+    /**
+     * 反序列化（字节数组 -> 对象）
+     */
+    @Override
+    public <T> T deserialize(byte[] data, Class<T> cls) {
+        logger.info("execute protostuff deserialize...");
+        if (data == null){
+            throw new SerializerException("deserialize data is null");
+        }
+        try {
+            T message = (T) objenesis.newInstance(cls);
+            Schema<T> schema = getSchema(cls);
+            ProtostuffIOUtil.mergeFrom(data, message, schema);
+            return message;
+        } catch (Exception e) {
+            throw new SerializerException(e.getMessage(), e);
+        }
+    }
+}
