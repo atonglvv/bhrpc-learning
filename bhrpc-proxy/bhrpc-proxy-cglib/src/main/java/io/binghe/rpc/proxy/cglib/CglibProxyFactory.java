415d5f0355a3865260af2c32eaf1d7cbd72731a3
binghe001 <1028386804@qq.com>
2022-11-08 21:34:01 +0800

    第33章：基于SPI扩展CGLib动态代理机制

---
+++ bhrpc-proxy/bhrpc-proxy-cglib/src/main/java/io/binghe/rpc/proxy/cglib/CglibProxyFactory.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2020-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.rpc.proxy.cglib;
+
+import io.binghe.rpc.proxy.api.BaseProxyFactory;
+import io.binghe.rpc.proxy.api.ProxyFactory;
+import io.binghe.rpc.spi.annotation.SPIClass;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.InvocationHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author binghe(公众号：冰河技术)
+ * @version 1.0.0
+ * @description Cglib动态代理
+ */
+@SPIClass
+public class CglibProxyFactory <T> extends BaseProxyFactory<T> implements ProxyFactory {
+    private final Logger logger = LoggerFactory.getLogger(CglibProxyFactory.class);
+    private final Enhancer enhancer = new Enhancer();
+    @Override
+    public <T> T getProxy(Class<T> clazz) {
+        logger.info("基于CGLib动态代理...");
+        enhancer.setInterfaces(new Class[]{clazz});
+        enhancer.setCallback(new InvocationHandler() {
+            @Override
+            public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+                return objectProxy.invoke(o, method, objects);
+            }
+        });
+        return (T) enhancer.create();
+    }
+}
