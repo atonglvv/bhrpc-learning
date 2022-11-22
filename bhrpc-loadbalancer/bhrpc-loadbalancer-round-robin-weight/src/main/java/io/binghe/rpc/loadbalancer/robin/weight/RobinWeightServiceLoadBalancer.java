e363993f147e8f09dc66477bc1a24f53512a87dd
binghe001 <1028386804@qq.com>
2022-11-22 13:09:57 +0800

    第45章：基于SPI扩展加权轮询算法负载均衡策略

--- bhrpc-loadbalancer/bhrpc-loadbalancer-round-robin-weight/src/main/java/io/binghe/rpc/loadbalancer/robin/weight/RobinWeightServiceLoadBalancer.java
+++ bhrpc-loadbalancer/bhrpc-loadbalancer-round-robin-weight/src/main/java/io/binghe/rpc/loadbalancer/robin/weight/RobinWeightServiceLoadBalancer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.binghe.rpc.loadbalancer.robin;
+package io.binghe.rpc.loadbalancer.robin.weight;
 
 import io.binghe.rpc.loadbalancer.api.ServiceLoadBalancer;
 import io.binghe.rpc.spi.annotation.SPIClass;
@@ -26,19 +26,23 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * @author binghe(公众号：冰河技术)
  * @version 1.0.0
- * @description 基于轮询算法的负载均衡策略
+ * @description 加权轮询负载均衡
  */
 @SPIClass
-public class RobinServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
-    private final Logger logger = LoggerFactory.getLogger(RobinServiceLoadBalancer.class);
+public class RobinWeightServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
+    private final Logger logger = LoggerFactory.getLogger(RobinWeightServiceLoadBalancer.class);
     private volatile AtomicInteger atomicInteger = new AtomicInteger(0);
     @Override
     public T select(List<T> servers, int hashCode) {
-        logger.info("基于轮询算法的负载均衡策略...");
+        logger.info("基于加权轮询算法的负载均衡策略...");
         if (servers == null || servers.isEmpty()){
             return null;
         }
-        int count = servers.size();
+        hashCode = Math.abs(hashCode);
+        int count = hashCode % servers.size();
+        if (count <= 0){
+            count = servers.size();
+        }
         int index = atomicInteger.incrementAndGet();
         if (index >= Integer.MAX_VALUE - 10000){
             atomicInteger.set(0);
