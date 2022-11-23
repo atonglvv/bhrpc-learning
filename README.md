a1da7b8e721dd136a7c6aa1ad6057d08a8ef4009
binghe001 <1028386804@qq.com>
2022-11-23 12:20:07 +0800

    第46章：基于SPI扩展Hash算法负载均衡策略

--- README.md
+++ README.md
@@ -176,3 +176,4 @@ bhrpc框架支持如下功能：
 * [《RPC手撸专栏》第43章：基于SPI扩展加权随机算法负载均衡策略](https://articles.zsxq.com/id_ejhf3a6a8hfi.html)
 * [《RPC手撸专栏》第44章：基于SPI扩展轮询算法负载均衡策略](https://articles.zsxq.com/id_nxu39tr84qpm.html)
 * [《RPC手撸专栏》第45章：基于SPI扩展加权轮询算法负载均衡策略](https://articles.zsxq.com/id_7jfkz4bfloaw.html)
+* [《RPC手撸专栏》第46章：基于SPI扩展Hash算法负载均衡策略](https://articles.zsxq.com/id_g7vt9ech3vx7.html)
---
+++ bhrpc-loadbalancer/bhrpc-loadbalancer-hash/src/main/java/io/binghe/rpc/loadbalancer/hash/HashServiceLoadBalancer.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2020-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.rpc.loadbalancer.hash;
+
+import io.binghe.rpc.loadbalancer.api.ServiceLoadBalancer;
+import io.binghe.rpc.spi.annotation.SPIClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * @author binghe(公众号：冰河技术)
+ * @version 1.0.0
+ * @description 基于Hash算法的负载均衡策略
+ */
+@SPIClass
+public class HashServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
+    private final Logger logger = LoggerFactory.getLogger(HashServiceLoadBalancer.class);
+    @Override
+    public T select(List<T> servers, int hashCode) {
+        logger.info("基于Hash算法的负载均衡策略...");
+        if (servers == null || servers.isEmpty()){
+            return null;
+        }
+        int index = Math.abs(hashCode) % servers.size();
+        return servers.get(index);
+    }
+}
