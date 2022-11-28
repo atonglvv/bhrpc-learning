14fc9d6731a283d07b4ef70eb9845b979370f4dc
binghe001 <1028386804@qq.com>
2022-11-28 11:38:03 +0800

    第49章：基于SPI扩展源IP地址加权Hash算法负载均衡策略

---
+++ bhrpc-loadbalancer/bhrpc-loadbalancer-sourceip-hash-weight/src/main/java/io/binghe/rpc/loadbalancer/sourceip/hash/weight/SourceIpHashWeightServiceLoadBalancer.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2020-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.rpc.loadbalancer.sourceip.hash.weight;
+
+import io.binghe.rpc.common.utils.StringUtils;
+import io.binghe.rpc.loadbalancer.api.ServiceLoadBalancer;
+import io.binghe.rpc.spi.annotation.SPIClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * @author binghe(公众号：冰河技术)
+ * @version 1.0.0
+ * @description 基于源IP地址加权Hash的负载均衡策略
+ */
+@SPIClass
+public class SourceIpHashWeightServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
+    private final Logger logger = LoggerFactory.getLogger(SourceIpHashWeightServiceLoadBalancer.class);
+    @Override
+    public T select(List<T> servers, int hashCode, String sourceIp) {
+        logger.info("基于源IP地址加权Hash的负载均衡策略...");
+        if (servers == null || servers.isEmpty()){
+            return null;
+        }
+        //传入的IP地址为空，则默认返回第一个服务实例m
+        if (StringUtils.isEmpty(sourceIp)){
+            return servers.get(0);
+        }
+        int count = Math.abs(hashCode) % servers.size();
+        if (count == 0){
+            count = servers.size();
+        }
+        int resultHashCode = Math.abs(sourceIp.hashCode() + hashCode);
+        return servers.get(resultHashCode % count);
+    }
+}
