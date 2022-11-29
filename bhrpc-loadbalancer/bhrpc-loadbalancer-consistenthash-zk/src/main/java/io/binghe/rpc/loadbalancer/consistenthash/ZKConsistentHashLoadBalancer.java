01c0210dd0fafe6ee381274e6db13cdd2047ca6b
binghe001 <1028386804@qq.com>
2022-11-29 11:12:33 +0800

    第50章：基于SPI扩展Zookeeper的一致性Hash算法负载均衡策略

---
+++ bhrpc-loadbalancer/bhrpc-loadbalancer-consistenthash-zk/src/main/java/io/binghe/rpc/loadbalancer/consistenthash/ZKConsistentHashLoadBalancer.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright 2020-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.rpc.loadbalancer.consistenthash;
+
+import io.binghe.rpc.loadbalancer.api.ServiceLoadBalancer;
+import io.binghe.rpc.protocol.meta.ServiceMeta;
+import io.binghe.rpc.spi.annotation.SPIClass;
+import org.apache.curator.x.discovery.ServiceInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author binghe(公众号：冰河技术)
+ * @version 1.0.0
+ * @description 基于Zookeeper的一致性Hash
+ */
+@SPIClass
+public class ZKConsistentHashLoadBalancer implements ServiceLoadBalancer<ServiceInstance<ServiceMeta>> {
+
+    private final static int VIRTUAL_NODE_SIZE = 10;
+    private final static String VIRTUAL_NODE_SPLIT = "#";
+
+    private final Logger logger = LoggerFactory.getLogger(ZKConsistentHashLoadBalancer.class);
+
+    @Override
+    public ServiceInstance<ServiceMeta> select(List<ServiceInstance<ServiceMeta>> servers, int hashCode, String ip) {
+        logger.info("基于Zookeeper的一致性Hash算法的负载均衡策略...");
+        TreeMap<Integer, ServiceInstance<ServiceMeta>> ring = makeConsistentHashRing(servers);
+        return allocateNode(ring, hashCode);
+    }
+
+    private ServiceInstance<ServiceMeta> allocateNode(TreeMap<Integer, ServiceInstance<ServiceMeta>> ring, int hashCode) {
+        Map.Entry<Integer, ServiceInstance<ServiceMeta>> entry = ring.ceilingEntry(hashCode);
+        if (entry == null) {
+            entry = ring.firstEntry();
+        }
+        if (entry == null){
+            throw new RuntimeException("not discover useful service, please register service in registry center.");
+        }
+        return entry.getValue();
+    }
+
+    private TreeMap<Integer, ServiceInstance<ServiceMeta>> makeConsistentHashRing(List<ServiceInstance<ServiceMeta>> servers) {
+        TreeMap<Integer, ServiceInstance<ServiceMeta>> ring = new TreeMap<>();
+        for (ServiceInstance<ServiceMeta> instance : servers) {
+            for (int i = 0; i < VIRTUAL_NODE_SIZE; i++) {
+                ring.put((buildServiceInstanceKey(instance) + VIRTUAL_NODE_SPLIT + i).hashCode(), instance);
+            }
+        }
+        return ring;
+    }
+
+    private String buildServiceInstanceKey(ServiceInstance<ServiceMeta> instance) {
+        ServiceMeta payload = instance.getPayload();
+        return String.join(":", payload.getServiceAddr(), String.valueOf(payload.getServicePort()));
+    }
+}
