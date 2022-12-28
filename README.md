e6025e1a290d4db912ce082ee4e708d955228bb0
binghe001 <1028386804@qq.com>
2022-12-28 11:12:47 +0800

    第70章：基于Spring注解接入服务提供者

--- README.md
+++ README.md
@@ -201,3 +201,4 @@ bhrpc框架支持如下功能：
 * [《RPC手撸专栏》第67章：服务消费者连接服务提供者的重试机制](https://articles.zsxq.com/id_i5r441hvg5fp.html)
 * [《RPC手撸专栏》第68章：服务提供者整合Spring](https://articles.zsxq.com/id_lpdm92w0kp2l.html)
 * [《RPC手撸专栏》第69章：基于Spring XML接入服务提供者](https://articles.zsxq.com/id_uah3g65dl7f7.html)
+* [《RPC手撸专栏》第70章：基于Spring注解接入服务提供者](https://articles.zsxq.com/id_punfttiew3sz.html)
---
+++ bhrpc-demo/bhrpc-demo-spring-annotation-provider/src/main/java/io/binghe/rpc/demo/spring/annotation/provider/config/SpringAnnotationProviderConfig.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2020-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.rpc.demo.spring.annotation.provider.config;
+
+import io.binghe.rpc.provider.spring.RpcSpringServer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @author binghe(公众号：冰河技术)
+ * @version 1.0.0
+ * @description 基于注解的配置类
+ */
+@Configuration
+@ComponentScan(value = {"io.binghe.rpc.demo"})
+@PropertySource(value = {"classpath:rpc.properties"})
+public class SpringAnnotationProviderConfig {
+
+    @Value("${registry.address}")
+    private String registryAddress;
+
+    @Value("${registry.type}")
+    private String registryType;
+
+    @Value("${registry.loadbalance.type}")
+    private String registryLoadbalanceType;
+
+    @Value("${server.address}")
+    private String serverAddress;
+
+    @Value("${reflect.type}")
+    private String reflectType;
+
+    @Value("${server.heartbeatInterval}")
+    private int heartbeatInterval;
+
+    @Value("${server.scanNotActiveChannelInterval}")
+    private int scanNotActiveChannelInterval;
+
+    @Bean
+    public RpcSpringServer rpcSpringServer(){
+        return new RpcSpringServer(serverAddress, registryAddress, registryType, registryLoadbalanceType, reflectType, heartbeatInterval, scanNotActiveChannelInterval);
+    }
+}
