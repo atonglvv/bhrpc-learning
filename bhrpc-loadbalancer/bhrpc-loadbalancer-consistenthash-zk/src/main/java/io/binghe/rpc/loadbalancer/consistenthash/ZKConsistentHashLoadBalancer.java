ab82b4f1fdcf4cfea5d7715d6d072cefa4620d9b
binghe001 <1028386804@qq.com>
2023-02-03 12:04:57 +0800

    修复Zk一致性Hash负载均衡泛型不匹配的问题

--- bhrpc-loadbalancer/bhrpc-loadbalancer-consistenthash-zk/src/main/java/io/binghe/rpc/loadbalancer/consistenthash/ZKConsistentHashLoadBalancer.java
+++ bhrpc-loadbalancer/bhrpc-loadbalancer-consistenthash-zk/src/main/java/io/binghe/rpc/loadbalancer/consistenthash/ZKConsistentHashLoadBalancer.java
@@ -16,14 +16,13 @@
 package io.binghe.rpc.loadbalancer.consistenthash;
 
 import io.binghe.rpc.loadbalancer.api.ServiceLoadBalancer;
-import io.binghe.rpc.protocol.meta.ServiceMeta;
 import io.binghe.rpc.spi.annotation.SPIClass;
-import org.apache.curator.x.discovery.ServiceInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.TreeMap;
 
 /**
@@ -32,7 +31,7 @@ import java.util.TreeMap;
  * @description 基于Zookeeper的一致性Hash
  */
 @SPIClass
-public class ZKConsistentHashLoadBalancer implements ServiceLoadBalancer<ServiceInstance<ServiceMeta>> {
+public class ZKConsistentHashLoadBalancer<T> implements ServiceLoadBalancer<T> {
 
     private final static int VIRTUAL_NODE_SIZE = 10;
     private final static String VIRTUAL_NODE_SPLIT = "#";
@@ -40,14 +39,14 @@ public class ZKConsistentHashLoadBalancer implements ServiceLoadBalancer<Service
     private final Logger logger = LoggerFactory.getLogger(ZKConsistentHashLoadBalancer.class);
 
     @Override
-    public ServiceInstance<ServiceMeta> select(List<ServiceInstance<ServiceMeta>> servers, int hashCode, String ip) {
+    public T select(List<T> servers, int hashCode, String ip) {
         logger.info("基于Zookeeper的一致性Hash算法的负载均衡策略...");
-        TreeMap<Integer, ServiceInstance<ServiceMeta>> ring = makeConsistentHashRing(servers);
+        TreeMap<Integer, T> ring = makeConsistentHashRing(servers);
         return allocateNode(ring, hashCode);
     }
 
-    private ServiceInstance<ServiceMeta> allocateNode(TreeMap<Integer, ServiceInstance<ServiceMeta>> ring, int hashCode) {
-        Map.Entry<Integer, ServiceInstance<ServiceMeta>> entry = ring.ceilingEntry(hashCode);
+    private T allocateNode(TreeMap<Integer, T> ring, int hashCode) {
+        Map.Entry<Integer, T> entry = ring.ceilingEntry(hashCode);
         if (entry == null) {
             entry = ring.firstEntry();
         }
@@ -57,9 +56,9 @@ public class ZKConsistentHashLoadBalancer implements ServiceLoadBalancer<Service
         return entry.getValue();
     }
 
-    private TreeMap<Integer, ServiceInstance<ServiceMeta>> makeConsistentHashRing(List<ServiceInstance<ServiceMeta>> servers) {
-        TreeMap<Integer, ServiceInstance<ServiceMeta>> ring = new TreeMap<>();
-        for (ServiceInstance<ServiceMeta> instance : servers) {
+    private TreeMap<Integer, T> makeConsistentHashRing(List<T> servers) {
+        TreeMap<Integer, T> ring = new TreeMap<>();
+        for (T instance : servers) {
             for (int i = 0; i < VIRTUAL_NODE_SIZE; i++) {
                 ring.put((buildServiceInstanceKey(instance) + VIRTUAL_NODE_SPLIT + i).hashCode(), instance);
             }
@@ -67,8 +66,7 @@ public class ZKConsistentHashLoadBalancer implements ServiceLoadBalancer<Service
         return ring;
     }
 
-    private String buildServiceInstanceKey(ServiceInstance<ServiceMeta> instance) {
-        ServiceMeta payload = instance.getPayload();
-        return String.join(":", payload.getServiceAddr(), String.valueOf(payload.getServicePort()));
+    private String buildServiceInstanceKey(T instance) {
+        return Objects.toString(instance);
     }
 }
